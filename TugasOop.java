9abd38a7c773712c5601e3ae41ecd0357f878268
murnikurniasari99 <48238994+murnikurniasari99@users.noreply.github.com>
2019-03-05 20:58:30 +0700

    17090013 - Murni K (4D)
    File Java OOP1

---
+++ TugasOop.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.oop;
+
+/**
+ *
+ * @author Murni K
+ */
+public class TugasOop {
+
+    public static void main(String[] args) {
+        System.out.println("Perkenalkan Nama Saya Murni Kurniasari dari 4D");
+    }
+    
+}
